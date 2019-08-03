34309616f37a40c147e624624c322f7cc79c2353
Hanqing1996 <zhq199622@163.com>
2019-08-03 17:15:02 +0800

    add

---
+++ 你真的懂函数吗/三种函数.js
@@ -0,0 +1,17 @@
+// 匿名函数
+var fn=function()
+{
+    console.log('1')
+}
+
+fn2=fn; // 指向同一个函数
+console.log(fn2.name); // 是匿名函数，但有name
+
+// 具名函数
+var fn1=function fn()
+{
+    console.log('1')
+}
+
+fn1()//1
+fn()//undefined
