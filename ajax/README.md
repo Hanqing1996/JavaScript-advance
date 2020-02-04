3ef986aeb93277b6156a31cff4167234dd2f310a
zhq <zhq199622@163.com>
2020-02-04 17:19:56 +0800

    update

--- ajax/README.md
+++
@@ -1,3 +0,0 @@
-#### 用 ajax 上传/下载文件
-* [上传]
-![image](https://github.com/Hanqing1996/JavaScript-advance/blob/master/Chrome/GIF/uu.gif)
--- ajax/uploadAndDownload/index.js
+++ ajax/uploadAndDownload/index.js
@@ -1,16 +1,40 @@
-const express=require('express')
+const express = require('express')
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
 const cors = require('cors')
+const upload = multer({ dest: 'uploads/' })
+const path = require('path')
 
-const app=express()
+const app = express()
+
 app.use(cors()) // 允许跨域
 
-app.post('/upload', upload.single('xxx'), function (req, res) {
-    res.json({key: req.file})
-})
-app.get('/hh',(req,res)=>{
-    res.send('hello node.js')
+// 这里的 'file' 与 form.html 里的 'file' 对应
+app.post('/upload',  upload.single('uploadFile'), function (req, res, next) {
+
+    res.end(req.file.filename)
 })
 
-app.listen(3000)
+
+app.get('/download/:name', function (req, res, next) {
+    console.log(`服务器响应 download 时间：第${new Date().getMilliseconds()}毫秒`);
+    var options = {
+        root: path.join(__dirname, 'uploads'),
+        headers: {
+            'Content-Type': 'image/jpeg',
+        }
+    }
+
+    var fileName = req.params.name
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            next(err)
+        } else {
+            console.log('Sent:', fileName)
+        }
+    })
+})
+
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
